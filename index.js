1d7f9625c1522761d47c864f851435d60f0bed8f
Víctor Rodríguez <vrodriguezbarbeira@gmail.com>
2023-10-03 16:18:30 +0200

    first commit.

---
+++ index.js
@@ -0,0 +1,26 @@
+alert("js cargado")
+
+let boton=document.getElementById("btnSubmit");
+boton.addEventListener("click", evento=>{
+            registrarPelicula();
+});
+
+
+let registrarPelicula = async()=>{
+
+    let fields = {};
+
+    fields.title=document.getElementById("title").value;
+    fields.director=document.getElementById("director").value;
+    fields.genre=document.getElementById("genre").value;
+
+    const peticion= await fetch("http://localhost:8080/api/peliculas",
+    {
+        method:"POST",  
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(fields)        
+    });
+}

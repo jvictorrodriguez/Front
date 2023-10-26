0106ba9dd40dc3a498e44702a922ea641ed86107
Víctor Rodríguez <vrodriguezbarbeira@gmail.com>
2023-10-26 23:03:29 +0200

    login and creatin users

--- IlernaProject/login&register/login.js
+++ IlernaProject/login&register/login.js
@@ -2,47 +2,64 @@ let title = document.getElementById("title");
 let signUp = document.getElementById("signUp");
 let signIn = document.getElementById("signIn");
 let nameInput = document.getElementById("nameInput");
-
+let $name = document.getElementById("name");
 let $username = document.getElementById("username");
 let $password = document.getElementById("password");
-let $recibido= document.getElementById("recibido");
+let $mensaje = document.getElementById("error");
+
+$name.addEventListener("click", () => {
+    if ($mensaje.length > 0) {
+        $name.innerHTML = "";
+    }
+    $mensaje.innerHTML = "";
+})
+
+$username.addEventListener("click", () => {
+    $mensaje.innerHTML = "";
+})
+$password.addEventListener("click", () => {
+    $mensaje.innerHTML = "";
+})
 
 
+signIn.addEventListener("click", () => {
 
-signIn.addEventListener("click", function (event) {
     const $username = document.getElementById("username").value;
     if ($username.length != 0) {
-        
-        ejecuta(this.value)
-
+        logInFunction(this.value)
     }
-
     nameInput.style.maxHeight = "0px";
     title.innerHTML = "Login";
     signUp.classList.add("disable");
     signIn.classList.remove("disable");
-
-   
-
 })
 
 signUp.addEventListener("click", () => {
     nameInput.style.maxHeight = "60px";
     title.innerHTML = "Registro";
+
+    if ($mensaje.innerHTML.length > 0) {
+        alert($mensaje.innerHTML);
+        $name.value = "";
+        $username.value = "";
+        $password.value = "";
+
+    }
+    $mensaje.innerHTML = "";
     signIn.classList.add("disable");
     signUp.classList.remove("disable");
+    
+    sendFile();
 })
 
 
 
 
-function ejecuta(valor) {
-    
-    let username=$username.value;
-    let password=$password.value;
-    //let username="admin";
-    //let password="admin";
-    let auth= btoa(`${username}:${password}`);
+function logInFunction(valor) {
+
+    let username = $username.value;
+    let password = $password.value;
+    let auth = btoa(`${username}:${password}`);
 
     var xhr = new XMLHttpRequest();
     var recibido = "";
@@ -50,16 +67,59 @@ function ejecuta(valor) {
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             recibido = xhr.responseText;
-            $recibido.innerHTML = recibido;
-            console.log(recibido);
-            alert(recibido);
+            if (recibido.length > 0) {
+                $mensaje.innerHTML = recibido;
+            }
+        }
+
+        if (this.status == 401) {
+            $mensaje.innerHTML = "Contraseña errónea";
         }
     }
 
-    xhr.open("GET","http://localhost:9001/login", true);
+    xhr.open("GET", "http://localhost:8080/login", true);
     xhr.setRequestHeader("Authorization", "Basic " + `${auth}`);
     xhr.send();
-    
+
 
 }
+
+
+
+    let sendFile = async () => {
+
+        alert("FunctionRegistro")
+        let username = $username.value;
+        let password = $password.value;
+        let fullName = $name.value;
+    
+
+        const usuarioDto = {
+            name: fullName,
+            username: username,
+            password: password
+        }
+
+
+
+        console.log(JSON.stringify(usuarioDto));
+
+
+
+
+
+        const peticion = await fetch("http://localhost:8080/register",
+            {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(usuarioDto)
+            });
+
+
+
+    }
+
 

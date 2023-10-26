34cc60ebd126509d010bbf91206af01117929960
v.rodriguez <vrodriguezbarbeira@gmail.com>
2023-10-26 14:07:14 +0200

    investigando..

--- IlernaProject/login&register/login.js
+++ IlernaProject/login&register/login.js
@@ -1,31 +1,65 @@
 let title = document.getElementById("title");
-let signUp= document.getElementById("signUp");
-let signIn= document.getElementById("signIn");
+let signUp = document.getElementById("signUp");
+let signIn = document.getElementById("signIn");
 let nameInput = document.getElementById("nameInput");
 
+let $username = document.getElementById("username");
+let $password = document.getElementById("password");
+let $recibido= document.getElementById("recibido");
 
 
 
-signIn.addEventListener("click",function(event){
-    const $username= document.getElementById("username").value;
-    if ($username.length==0) {
-        alert("vacío");
-        event.preventDefault()
+signIn.addEventListener("click", function (event) {
+    const $username = document.getElementById("username").value;
+    if ($username.length != 0) {
+        
+        ejecuta(this.value)
+
     }
-    
-    nameInput.style.maxHeight="0px";
-    title.innerHTML="Login";
+
+    nameInput.style.maxHeight = "0px";
+    title.innerHTML = "Login";
     signUp.classList.add("disable");
     signIn.classList.remove("disable");
+
+   
+
 })
 
-signUp.addEventListener("click", ()=>{
-    nameInput.style.maxHeight="60px";
-    title.innerHTML="Registro";
+signUp.addEventListener("click", () => {
+    nameInput.style.maxHeight = "60px";
+    title.innerHTML = "Registro";
     signIn.classList.add("disable");
     signUp.classList.remove("disable");
 })
 
-document.getElementById("myAnchor").addEventListener("click", function(event){
-    event.preventDefault()
-  });
+
+
+
+function ejecuta(valor) {
+    
+    let username=$username.value;
+    let password=$password.value;
+    //let username="admin";
+    //let password="admin";
+    let auth= btoa(`${username}:${password}`);
+
+    var xhr = new XMLHttpRequest();
+    var recibido = "";
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            recibido = xhr.responseText;
+            $recibido.innerHTML = recibido;
+            console.log(recibido);
+            alert(recibido);
+        }
+    }
+
+    xhr.open("GET","http://localhost:9001/login", true);
+    xhr.setRequestHeader("Authorization", "Basic " + `${auth}`);
+    xhr.send();
+    
+
+}
+
--- OtrasPruebas/registro.js
+++ OtrasPruebas/registro.js
@@ -1,6 +1,7 @@
 let $username = document.getElementById("username");
 let $password = document.getElementById("password");
 let $button=document.getElementById("submit");
+let $resultado= document.getElementById("resultado");
 
 
  
@@ -18,11 +19,11 @@ function ejecuta(){
     xhr.onreadystatechange = function(){
         if (this.readyState == 4 && this.status==200){
             recibido= xhr.responseText;
-           resultado.innerHTML = recibido;
+           $resultado.innerHTML = recibido;
         }
     }
 
-    xhr.open("GET","http://localhost:9000/login", true);
+    xhr.open("GET","http://localhost:9001/login", true);
     xhr.setRequestHeader("Authorization", "Basic " + `${auth}`);
     xhr.send();
     

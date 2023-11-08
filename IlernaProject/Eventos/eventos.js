a4ab799fa24e622118244be0565de97f550265dc
v.rodriguez <vrodriguezbarbeira@gmail.com>
2023-11-08 13:58:37 +0100

    Podemos visualizar los eventos

--- IlernaProject/Eventos/eventos.js
+++ IlernaProject/Eventos/eventos.js
@@ -1,39 +1,20 @@
+/*************** V A R I A B L E S ********************/
+let jwtoken="";
 
-function ejecuta() {
 
-        var xhr = new XMLHttpRequest();
-        var recibido = "";
 
-        xhr.onload = function () {
-                recibido = xhr.json;
-                resultado.innerHTML = recibido;
-
-        }
-        let jwtoken = readCookie("token");
-        xhr.open("GET", "http://localhost:9090/event/all");
-        xhr.setRequestHeader('Authorization', 'Bearer ' + jwtoken);
-        xhr.send();
+window.onload = function () {
+        listarEventos();
+    
 }
 
 
 
-function readCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-}
 
 let listarEventos = async () => {
 
-        let jwtoken = "";
+        
         jwtoken = readCookie("token");
-        console.log(jwtoken);
-
 
         const peticion = await fetch("http://localhost:9090/event/all",
                 {
@@ -61,7 +42,16 @@ let listarEventos = async () => {
         document.querySelector("#tabla tbody").outerHTML = contenidoTabla;
 
 }
-window.onload = function () {
-        listarEventos();
-    
+
+
+function readCookie(name) {
+        //webgrafia https://www.quirksmode.org/js/cookies.html#script
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
 }

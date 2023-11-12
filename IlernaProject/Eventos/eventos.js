a73bbe2c2d20bbb9b344d347786f6a3c09a5c4d5
Víctor Rodríguez <vrodriguezbarbeira@gmail.com>
2023-11-12 22:01:33 +0100

    -

--- IlernaProject/Eventos/eventos.js
+++ IlernaProject/Eventos/eventos.js
@@ -1,10 +1,78 @@
 /*************** V A R I A B L E S ********************/
-let jwtoken = "";
+let jwtoken = readCookie("token");
+let host = "http://localhost:9090"
+let eventId;
 
+let $description = document.getElementById("description");
+let $date = document.getElementById("date");
+let $fechas=document.getElementById("fechas");
+let $addDate= document.getElementById("addDate"); //button
+let $place = document.getElementById("place");
+let $price = document.getElementById("price");
+
+
+let dates={};
+let fecha=[];
+
+let $enviar = document.getElementById("enviar");
+
+/****************LISTENERS******************/
 
 window.onload = function () {
         listarEventos();
 }
+
+$addDate.addEventListener("click",()=>{
+        addDateToList()
+    });
+    
+
+$enviar.addEventListener("click", ()=> {
+        altaEvento();
+})
+
+
+
+function addDateToList(){    
+
+        $fechas.innerHTML+=calendar.value +"<br>";
+        fecha.push(calendar.value);
+}
+
+
+
+function addUserToEvent(){
+
+        
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", host + "/addUserToEvent/"+eventId)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.setRequestHeader( 'Authorization', 'Bearer ' + jwtoken)
+        xhr.send(JSON.stringify(json))
+
+}
+
+function altaEvento() {
+
+        const json = {
+                description: $description.value,
+                date: fecha,
+                place: $place.value,
+                price: $price.value,
+        };
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", host + "/event")
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.setRequestHeader( 'Authorization', 'Bearer ' + jwtoken)
+
+
+        // send rquest with JSON payload
+        xhr.send(JSON.stringify(json))
+
+        //necesito recuperar el id para almacenarlo en eventId;
+}
+
 
 
 let listarEventos = async () => {
